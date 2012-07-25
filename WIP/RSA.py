d0ac5012864415f12e8796a0e9a55c24706672cf
Blake Dutton <blakedutton@optusnet.com.au>
2012-07-25 21:21:17 +1000

    Annotation

--- WIP/RSA.py
+++ WIP/RSA.py
@@ -20,18 +20,26 @@ q = input ("Please enter var q: ")
 if not isPrime(q):
     print "P was not a prime number, please start again."
     sys.exit()
+n = pq #n is the modulus for the public and private keys
+#Calculate the Euler Totient of n
 x = (p-1)*(q-1)
+#Generate a random coprime e for x
+#e is the public key
 e = random.randint(1, x)
 while fractions.gcd(e, x)!=1:
     e = random.randint(1, x)
+#Create the private key d
 d = e ** -1
 print "Key's generated."
 m = input("Enter a number to encrypt: ")
 if isinstance(m, int) == False:
     print "Your entered string was not an integer, please start again."
     sys.exit()
+#Use the public key e to encrypt message c
+#!!!When sending a message to someone else, use THEIR public key
 c = m**e
 print "Your encrypted number is: ", c
 h = input("Enter the same number to decrypt it: ")
+#Use YOUR private key d to decrypt the message sent to you which was encrypted with YOUR public key
 b = c**d
 print "Your decrypted number is: ", b
