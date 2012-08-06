106b0343453ce0fab22f0ceba86860e0c98b62dd
Blake Dutton <blake@blake-TECRA-A6.(none)>
2012-08-06 18:13:41 +1000

    Added caesar WIP

---
+++ WIP/caesar.py
@@ -0,0 +1,15 @@
+import sys
+class Encryption:
+    def __init__(self):
+        pass
+    def encrypt(self, p):
+        pass
+    def decrypt(self, c):
+        pass
+class Conversion:
+    def __init__(self:
+        pass
+    def toText(self, i):
+        pass
+    def toInteger(self, t):
+        pass
