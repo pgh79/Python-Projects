362da7cf1f0184de6706d8c054ddcc0443fad4ba
blakedut2 <blakedutton@optusnet.com.au>
2012-08-07 11:46:15 +1000

    Remove redundant code

--- WIP/caesar.py
+++ WIP/caesar.py
@@ -1,6 +1,10 @@
 # Blake Dutton
 import sys
 import string
+#Define important Var's
+global key
+crypt = Encryption()
+cipher = ""
 # NB: p is plaintext and c is ciphertext
 class Encryption:
     def __init__(self):
@@ -18,44 +22,16 @@ class Encryption:
             return cstr
         else:
             return p
-    def decrypt(self, c):
-        if c.isupper():
-            cint = string.uppercase.index(c)
-            c = (cint-key)%26
-            pstr = string.uppercase[c]
-            return pstr
-        elif c.isalpha():
-            cint = string.lowercase.index(c)
-            c = (cint-key)%26
-            pstr = string.lowercase[c]
-            return pstr
-        else:
-            return c
-def interface():
-    crypt = Encryption()
-    choice = raw_input("Do you want to (e)ncrypt, (d)ecrypt or (q)uit? ")
-    if choice == "e":
-        cipher = ""
-        plain = raw_input("Enter a string to encrypt: ")
-        for char in plain:
-            n = crypt.encrypt(char)
-            cipher += n
-        print "Your encrypted text is:",cipher
-        interface()
-    elif choice == "d":
-        plain = ""
-        cipher = raw_input("Enter a string to decrypt: ")
-        for char in cipher:
-            n = crypt.decrypt(char)
-            plain += n
-        print "Your decrypted text is:",plain
-        interface()
-    elif choice == "q":
-        quit()
-global key
 if (len(sys.argv) < 2):
     print "No key was entered, TERMINATING!"
     quit()
+elif sys.argv[1] < 1:
+    print "The key entered was not a positive integer, TERMINATING!"
+    quit()
 else:
     key = int(sys.argv[1])
-    interface()
+    plain = raw_input("Enter a string to encrypt: ")
+    for char in plain:
+        char = crypt.encrypt(char)
+        cipher += char
+    print cipher
