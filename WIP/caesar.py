8990af54cf92eb7436c111c277c2d6ab69b10ed5
blakedut2 <blakedutton@optusnet.com.au>
2012-08-07 13:30:42 +1000

    Remove redundant code

--- WIP/caesar.py
+++ WIP/caesar.py
@@ -1,27 +1,22 @@
 # Blake Dutton
 import sys
 import string
-#Define important Var's
-global key
-crypt = Encryption()
-cipher = ""
 # NB: p is plaintext and c is ciphertext
-class Encryption:
-    def __init__(self):
-        pass
-    def encrypt(self, p):
-        if p.isupper():
-            pint = string.uppercase.index(p)
-            c = (pint+key)%26
-            cstr = string.uppercase[c]
-            return cstr
-        elif p.isalpha():
-            pint = string.lowercase.index(p)
-            c = (pint+key)%26
-            cstr = string.lowercase[c]
-            return cstr
-        else:
-            return p
+def encrypt(p):
+    if p.isupper():
+        pint = string.uppercase.index(p)
+        c = (pint+key)%26
+        cstr = string.uppercase[c]
+        return cstr
+    elif p.isalpha():
+        pint = string.lowercase.index(p)
+        c = (pint+key)%26
+        cstr = string.lowercase[c]
+        return cstr
+    else:
+        return p
+global key
+cipher = ""
 if (len(sys.argv) < 2):
     print "No key was entered, TERMINATING!"
     quit()
@@ -32,6 +27,6 @@ else:
     key = int(sys.argv[1])
     plain = raw_input("Enter a string to encrypt: ")
     for char in plain:
-        char = crypt.encrypt(char)
+        char = encrypt(char)
         cipher += char
     print cipher
